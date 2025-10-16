27058cb8532593cad6f4e8353d389824c5af08e8
Kamchy <kamchy2501@gmail.com>
2025-10-16 10:33:24 +0600

    Обновлена собранная статика с логотипом

--- src/settings.py
+++ src/settings.py
@@ -112,6 +112,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
